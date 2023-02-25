b4540fa8bfe5a41d534e5b185a41923570bc816b
jmcheon <jm@JM-MacBookPro.local>
2023-02-25 17:01:20 +0900

    Feat: 헤더 및 배경 설정

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
+    <div className="w-full h-screen bg-black text-white">
       <Header />
       <Outlet />
     </div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,24 @@
 import { Link, useNavigate } from "react-router-dom";
+
 function Header() {
   return (
-    <h1>
-      <li>
-        <Link to={"/"}>Home</Link>
-      </li>
-      <li>
-        <Link to={"/about"}>About</Link>
-      </li>
-    </h1>
+    <div className="w-full h-10 px-3 flex flex-row justify-between items-center">
+      <h1>JM's</h1>
+      <ul className="w-1/2 flex flex-row justify-evenly">
+        <li>
+          <Link to={"/"}>PROJECT</Link>
+        </li>
+        <li>
+          <Link to={"/about"}>ABOUT</Link>
+        </li>
+        <li>
+          <Link to={"/connect"}>CONNECT</Link>
+        </li>
+        <li>
+          <Link to={"/resume"}>RESUME</Link>
+        </li>
+      </ul>
+    </div>
   );
 }
 export default Header;
